12a2f047506e948f8b96ef0681733279ffb99747
Ezra-sqr <143028029+Ezra-sqr@users.noreply.github.com>
2024-04-25 22:20:33 -0700

    Update LightBoard.java

--- src/main/java/LightBoard.java
+++ src/main/java/LightBoard.java
@@ -10,7 +10,14 @@ public class LightBoard
    */
   public LightBoard(int numRows, int numCols)
   {
-    /* to be implemented in part (a) */
+    lights=new boolean[numRows][numCols];
+    for (int r=0; r<lights.length;r++)
+      for (int c=0; c<lights[r].length;c++){
+        if (Math.random()*10<=4)
+          lights[r][c]=true;
+        else
+          lights [r][c]= false;
+        }
 
   }
 
@@ -20,8 +27,16 @@ public class LightBoard
    */
   public boolean evaluateLight(int row, int col)
   {
-    /* to be implemented in part (b) */
-   
+    int rowOn=0;
+    for (int c=0;c<lights[row].length;c++)
+      if(lights[row][c]==true)
+        rowOn++;
+    if(lights[row][col]==true && rowOn%2==0)
+      return false;
+    else if (lights [row][col]==false && row on%3==0)
+       return true;
+    else
+      return lights [row][col];
  
   }
   public boolean[][] getLights()
